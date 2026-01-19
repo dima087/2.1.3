da23ccc3c160201016df22847d1d2c205b67e103
dima087 <dima_087@inbox.ru>
2026-01-19 16:34:30 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const gridContainer = document.getElementById('gridContainer');
+const toggleBtn = document.getElementById('toggleBtn');
+const image  = document.getElementById('img-end');
+
+
+let isCollapsed = false;
+
+toggleBtn.addEventListener('click', () => {
+    if (isCollapsed) {
+        gridContainer.classList.toggle('collapsed');
+        toggleBtn.textContent = 'Скрыть';
+        image.style.transform = 'rotate(0deg)'
+
+    } else {
+        gridContainer.classList.add('collapsed');
+        toggleBtn.textContent = 'Показать все';
+        image.style.transform = 'rotate(180deg)'
+    }
+    isCollapsed = !isCollapsed;
+});
+
+const swiper = new Swiper('.swiper', {
+    // Параметры
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
